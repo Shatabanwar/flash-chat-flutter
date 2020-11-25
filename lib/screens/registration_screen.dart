e65a00d778e73a2d678fc8edd999c087115a080a
Shatab Anwar <shatabanwar11@gmail.com>
2020-11-25 15:55:25 +0530

    Created Routes.

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class RegistrationScreen extends StatefulWidget {
+
+  static const String id = 'register';
+
+
   @override
   _RegistrationScreenState createState() => _RegistrationScreenState();
 }
